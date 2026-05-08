ad297e4e89d9e7cc440966ac966f9b517f8ac220
nakacy0919-Git <nakacy0919@gmail.com>
2026-05-08 16:35:26 +0900

    Fix: Add base path and update script src

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // リポジトリ名を / で囲んで指定します
+  // GitHubのリポジトリ名を指定（スラッシュで囲むのを忘れずに！）
   base: '/vocadash/', 
 })
