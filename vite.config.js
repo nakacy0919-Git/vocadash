350cceb1fe2661ae673b9010ff482c858c82a9ff
nakacy0919-Git <nakacy0919@gmail.com>
2026-05-09 08:16:05 +0900

    職場で〇〇を修正

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHubのリポジトリ名を指定（スラッシュで囲むのを忘れずに！）
-  base: '/vocadash/', 
+  base: '/vocadash/', // ← これを追加！（スラッシュで囲むのを忘れずに）
 })
