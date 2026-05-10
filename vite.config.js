a6211982a90b57b33314945e3b43a76b6dbee355
NAKACY0919 <NAKACY0919@GMAIL.com>
2026-05-10 09:34:32 +0900

    chore: Vercelデプロイ用にパスを修正

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vocadash/', // ← これを追加！（スラッシュで囲むのを忘れずに）
+  // ▼ ここを '/' に戻す
+  base: '/', 
 })
